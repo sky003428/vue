38a5be5a6a172ecaba9bbdd120ecc0b194e09274
Tommy <sky003428@gmail.com>
2022-07-08 15:35:43 +0800

    0708

--- rgb2hsv.js
+++ rgb2hsv.js
@@ -1,4 +1,4 @@
-export default function rgb2hsv(arr) {
+export function rgb2hsv(arr) {
     let rr;
     let gg;
     let bb;
@@ -36,5 +36,54 @@ export default function rgb2hsv(arr) {
     }
     return [Math.round(h * 360), Math.round(s * 100), Math.round(v * 100)]
 };
+
+export function hsv2rgb(hsv) {
+    let _l = hsv[0];
+    let _m = hsv[1];
+    let _n = hsv[2];
+    let newR;
+    let newG;
+    let newB;
+    if (_m === 0) {
+        _l = _m = _n = Math.round(255 * _n / 100);
+        newR = _l;
+        newG = _m;
+        newB = _n;
+    } else {
+        _m = _m / 100;
+        _n = _n / 100;
+        let p = Math.floor(_l / 60) % 6;
+        let f = _l / 60 - p;
+        let a = _n * (1 - _m);
+        let b = _n * (1 - _m * f);
+        let c = _n * (1 - _m * (1 - f));
+        switch (p) {
+            case 0:
+                newR = _n; newG = c; newB = a;
+                break;
+            case 1:
+                newR = b; newG = _n; newB = a;
+                break;
+            case 2:
+                newR = a; newG = _n; newB = c;
+                break;
+            case 3:
+                newR = a; newG = b; newB = _n;
+                break;
+            case 4:
+                newR = c; newG = a; newB = _n;
+                break;
+            case 5:
+                newR = _n; newG = a; newB = b;
+                break;
+        }
+        newR = Math.round(255 * newR);
+        newG = Math.round(255 * newG);
+        newB = Math.round(255 * newB);
+    }
+    return [newR, newG, newB]
+}
+
+
 
  
